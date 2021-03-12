0bb0cf2689bb598048215b79ea8f4c895698aa5b
KrasimiraVeleva <63585462+KrasimiraVeleva@users.noreply.github.com>
2021-03-12 16:30:07 +0200

    Add files via upload

--- Dwarf.java
+++ Dwarf.java
@@ -3,7 +3,7 @@ package pieces;
 import java.awt.*;
 import java.util.Random;
 
-public class Dwarf extends Figure{
+public class Dwarf extends Figure {
 
     public Dwarf(int row, int col) {
 
@@ -15,5 +15,36 @@ public class Dwarf extends Figure{
         this.speed     = 2;
     }
 
-    
+    /**
+     * Метод изобразяващ джуджетата на игралната дъска
+     * @param g Graphics object
+     */
+    public void render(Graphics g) {
+
+        int dwarf1 = random();
+        int dwarf2 = random();
+        int dwarf3 = random();
+        int dwarf4 = random();
+
+        g.setColor(Color.GREEN);
+        g.fillOval((dwarf1*100) + 15, 15,  70, 70);
+        g.fillOval((dwarf2*100) + 15, 115, 70, 70);
+
+        g.setColor(Color.GREEN);
+        g.fillOval((dwarf3*100) + 15, 515, 70, 70);
+        g.fillOval((dwarf4*100) + 15, 615, 70, 70);
+    }
+
+    /**
+     * Метод изобразяващ фигурите на случаен принцип
+     * @return
+     */
+    private int random() {
+        Random random = new Random();
+        int rand = random.nextInt(5);
+        while (rand == 2) {
+            rand = random.nextInt(5);
+        }
+        return rand;
+    }
 }
