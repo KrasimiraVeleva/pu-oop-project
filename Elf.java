bd49d4dae13629d4245db1bac573badbd0deedd7
KrasimiraVeleva <63585462+KrasimiraVeleva@users.noreply.github.com>
2021-03-12 16:13:48 +0200

    Add files via upload

--- Elf.java
+++ Elf.java
@@ -1,5 +1,8 @@
 package pieces;
 
+import java.awt.*;
+import java.util.Random;
+
 public class Elf extends Figure{
 
      public Elf(int row, int col) {
@@ -11,6 +14,30 @@ public class Elf extends Figure{
          this.attackBox = 3;
          this.speed     = 3;
      }
+     
+    private int random() {
+        Random random = new Random();
+        int rand = random.nextInt(5);
+        while (rand == 2) {
+            rand = random.nextInt(5);
+        }
+        return rand;
+    }
 
+    public void render(Graphics g) {
+
+        int elf1 = random();
+        int elf2 = random();
+        int elf3 = random();
+        int elf4 = random();
+
+        g.setColor(Color.RED);
+        g.fillOval((elf1*100) + 15, 15,  70, 70);
+        g.fillOval((elf2*100) + 15, 115, 70, 70);
+
+        g.setColor(Color.RED);
+        g.fillOval((elf3*100) + 15, 515,  70, 70);
+        g.fillOval((elf4*100) + 15, 615,  70, 70);
+    }
 
 }
